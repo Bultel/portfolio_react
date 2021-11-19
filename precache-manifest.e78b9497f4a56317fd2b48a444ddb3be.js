9573703aed849bc39a0f4fec9ef12f8d22029ffa
Bultel <bultelalexis@gmail.com>
2021-11-19 10:56:26 +0100

    Updates

---
+++ precache-manifest.e78b9497f4a56317fd2b48a444ddb3be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e8c4a39ac189be99086fc11dc2876f",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "df0dbd33b52d0e7e0eab",
+    "url": "/portfolio_react/static/css/main.90443bbb.chunk.css"
+  },
+  {
+    "revision": "70030cc72d33f50d2a54",
+    "url": "/portfolio_react/static/js/2.3c75086c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio_react/static/js/2.3c75086c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0dbd33b52d0e7e0eab",
+    "url": "/portfolio_react/static/js/main.699cbb1b.chunk.js"
+  },
+  {
+    "revision": "703275573a77d4679301",
+    "url": "/portfolio_react/static/js/runtime-main.d32fb8f0.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/portfolio_react/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/portfolio_react/static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
